84946d8f747aac837d832b5a23e387c089a2fb8d
Yusuke Ito <novi.mad@gmail.com>
2012-01-17 19:43:24 +0900

    Initial Commit

---
+++ LTGridView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LTGridView
+//
+//  Created by 伊藤 祐輔 on 12/01/17.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ LTGridView/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  LTGridView
+//
+//  Created by 伊藤 祐輔 on 12/01/17.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
